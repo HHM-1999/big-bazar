e0097670d9f01104fd81aad775eeb9a274729893
jahanur96 <jahanurcse96@gmail.com>
2023-08-22 18:08:01 +0600

    added header and main section

---
+++ common/js/script.js
@@ -0,0 +1,12 @@
+///sticky///
+const stickyNav = document.getElementById("sticky-navbar");
+
+function toggleStickyNavbar() {
+    if (window.scrollY > stickyNav.offsetTop) {
+        stickyNav.classList.add('sticky');
+
+    } else {
+        stickyNav.classList.remove("sticky");
+    }
+}
+window.addEventListener('scroll', toggleStickyNavbar);
