d8b9a4b0c758e0bc8ce870d001600527a7e63718
jahanur96 <jahanurcse96@gmail.com>
2023-08-29 16:17:44 +0600

    added sidebar

--- common/js/script.js
+++ common/js/script.js
@@ -12,3 +12,23 @@ function toggleStickyNavbar() {
 window.addEventListener('scroll', toggleStickyNavbar);
 
 
+//sidebar//
+function showSearchBox() {
+    var searchcontainer = document.getElementById("searchcontainer");
+    searchcontainer.classList.remove("hidden");
+    console.log("hello")
+}
+
+function hideSearchBox() {
+    var searchcontainer = document.getElementById("searchcontainer");
+    searchcontainer.classList.add("hidden");
+    console.log("close")
+}
+
+function performSearch() {
+    var searchInput = document.getElementById("searchInput").value;
+    // Perform your search operation using the search input value
+    // Add your search logic here (e.g., making an API request, filtering data)
+    console.log("Performing search: " + searchInput);
+
+}
